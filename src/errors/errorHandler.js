88802dff9c01fc05ae083baaab4f1e9ed4c2c596
vitomax <v.o.sazonov@gmail.com>
2021-11-12 19:16:35 +0300

    fix: add process.exit(code) in function errorHandler()

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -2,8 +2,7 @@ const errorHandler = (err) => {
   const { isCustom, code, message, name } = err;
   if (isCustom) {
     console.error(`${name}: ${message}`);
-    process.exitCode = code;
-    return;
+    process.exit(code);
   }
   throw err;
 };
